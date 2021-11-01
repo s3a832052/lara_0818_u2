38fa2af784d0523e370c53e6bda596b9d44fdd13
GuotingRui <azsxdc890902@gmail.com>
2021-11-02 01:38:29 +0800

    練習三

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('home',[HomeController::class,'index']);
+
+Route::get('hello/{name?}',[HelloController::class, 'index'])->name('hello.index');
 
